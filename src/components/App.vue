724ca371ae2cc5267552fcd191b472a23e25f6d6
Dylan Hassinger <d@dylanized.com>
2018-05-05 19:20:53 -0500

    implemented router

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>This is the App component</h1>
+  <main>
+    <ul>
+      <li><router-link to='/'>Home</router-link></li>
+      <li><router-link to='/about'>About</router-link></li>
+      <li><router-link to='/error'>Error</router-link></li>
+    </ul>
+    <router-view></router-view>
+  </main>
 </template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // load Vue library
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 // load components
 import App from './components/App';
@@ -7,11 +8,35 @@ import App from './components/App';
 // register components
 Vue.component('App', App);
 
+// load pages
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ErrorPage from './pages/ErrorPage';
+
+// register pages
+Vue.component('HomePage', HomePage);
+Vue.component('AboutPage', AboutPage);
+Vue.component('ErrorPage', ErrorPage);
+
 // disable dev msg
 Vue.config.productionTip = false;
 
+// load router middleware
+Vue.use(VueRouter);
+
+// instantiate the router and define routes
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/about', component: AboutPage },
+    { path: '*', component: ErrorPage },
+  ],
+});
+
 // launch App
 new Vue({
-  el: 'main',
+  router,
   template: '<app></app>',
-});
+}).$mount('main');
