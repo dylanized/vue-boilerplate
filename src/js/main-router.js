514785f2c0d2529c6ef797d3b0db37eded82480c
Dylan Hassinger <d@dylanized.com>
2018-05-15 15:29:58 -0500

    removed main-pages

--- src/js/main-router.js
+++ src/js/main-router.js
@@ -4,17 +4,19 @@ import VueRouter from 'vue-router';
 // load router middleware
 Vue.use(VueRouter);
 
-// load pages
-import pages from './main-pages.js';
+// load page components
+import HomePage from '@/inc/pages/HomePage';
+import AboutPage from '@/inc/pages/AboutPage';
+import ErrorPage from '@/inc/pages/ErrorPage';
 
 // instantiate the router and define routes
 const router = new VueRouter({
   mode: 'history',
   base: __dirname, // eslint-disable-line
   routes: [
-    { path: '/', component: pages.home },
-    { path: '/about', component: pages.about },
-    { path: '*', component: pages.error },
+    { path: '/', component: HomePage },
+    { path: '/about', component: AboutPage },
+    { path: '*', component: ErrorPage },
   ],
   linkActiveClass: 'active',
 });
