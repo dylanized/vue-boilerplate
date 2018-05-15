8f4e640cbcaeecb1de5be8029e39cfaa2b544170
Dylan Hassinger <d@dylanized.com>
2018-05-15 11:38:06 -0500

    setting exact attr

--- src/inc/layout/AppNav.vue
+++ src/inc/layout/AppNav.vue
@@ -3,7 +3,7 @@
     <div class='collapse navbar-collapse' id='navbarsExampleDefault'>
       <div class='col-md-4 col-12 p-1'>
         <ul class='navbar-nav mr-auto'>
-          <li v-for='link in links' class='nav-item'><router-link :to='link.to' class='nav-link'>{{ link.text }}</router-link></li>
+          <li v-for='link in links'><router-link :to='link.to' class='nav-link' exact>{{ link.text }}</router-link></li>
         </ul>
       </div>
     </div>
